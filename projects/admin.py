d6abe41c757d7f21c082dba292ac98dab2105fd4
Gilissen Steve <steve@pulseconsulting.be>
2020-02-12 01:10:15 +0100

    Added timesheet models

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('customer', )
+
+
+class TimeEntryInline(admin.TabularInline):
+    def unit(self, instance):
+        return str(instance.timesheet.project.billing_rate)
+    
+
+    readonly_fields = ('unit',)
+    model = models.TimeEntry
+    extra = 0
+
+
+@admin.register(models.Timesheet)
+class TimesheetAdmin(admin.ModelAdmin):
+    list_display = ('project', 'reference', 'consultant', 'start_date', 'end_date', )
+    inlines = [TimeEntryInline]
+
+
--- projects/models.py
+++ projects/models.py
@@ -1,25 +1,45 @@
 from django.db import models
-from decimal import Decimal
+from django.utils.translation import ugettext_lazy
 
 import datetime
-import time
 
-from django.utils.timezone import now
 from accounts.models import CompanyProfile, ConsultantProfile
 from customers.models import Customer
 
 
+BILLING_RATE_CHOICES = [
+    ('HR', ugettext_lazy('Hourly Rate')),
+    ('FD', ugettext_lazy('Flat Daily Rate')),
+    ('PR', ugettext_lazy('Per-Project Basis')),
+]
+
+
 # Create your models here.
 class Project(models.Model):
-    consultant = models.ForeignKey(ConsultantProfile, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     third_party_identifier = models.CharField(max_length=255, blank=True)
     start_date = models.DateField(default=datetime.date.today)
     end_date = models.DateField(blank=True, null=True)
     description = models.TextField(blank=True)
+    billing_rate = models.CharField(max_length=2, default='HR', choices=BILLING_RATE_CHOICES)
 
     def __str__(self):
         return self.name
 
 
+class Timesheet(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    reference = models.CharField(max_length=100, blank=True)
+    consultant = models.ForeignKey(ConsultantProfile, on_delete=models.CASCADE)
+    start_date = models.DateField(default=datetime.date.today)
+    end_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.project.name
+
+
+class TimeEntry(models.Model):
+    timesheet = models.ForeignKey(Timesheet, on_delete=models.CASCADE)
+    date = models.DateField()
+    units = models.DecimalField(max_digits=10, decimal_places=2)
